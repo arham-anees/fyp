153417018166c08f073c923a735505f5481b6adf
arham-anees <arhamanees10@gmail.com>
2020-04-05 14:33:47 +0500

    brand new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-  .example-icon {
-    padding: 0 14px;
-  }
-  
-  .example-spacer {
-    flex: 1 1 auto;
-  }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Change Monitor';
-
-  logout(){
-
-  }
-
-  flexContainer={
-    
-  }
+  title = 'fyp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { RegisterTeamComponent } from './register-team/register-team.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    SignUpComponent,
-    ForgetPasswordComponent,
-    ResetPasswordComponent,
-    RegisterTeamComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatToolbarModule,
-    LayoutModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
